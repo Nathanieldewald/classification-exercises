a54cbe32b425c40e99f68160637b5e3866d4c3d4
Nathaniel Dewald <dewald.nathaniel@gmail.com>
2023-04-14 15:41:36 -0500

    Updated classification_exercises.ipynb and acquire.py

--- acquire.py
+++ acquire.py
@@ -1,88 +1,95 @@
+import pandas as pd
 import os
 
-from env import host, username, password
-import pandas as pd
+from sklearn.model_selection import train_test_split
+
+import env
+import numpy as np
 
 
-def get_db_url(db_name):
-    '''
-    This function uses my info from a env.py file to create a url to access the Codeup db.
-    '''
-    from env import host, username, password
-    return f'mysql+pymysql://{username}:{password}@{host}/{db_name}'
-
-def new_titanic_data(sql_query):
-    '''
-    This function reads the titanic data from the Codeup db into a df.
-    '''
-    sql_query = 'SELECT * FROM passengers'
-    return pd.read_sql(sql_query, get_db_url('titanic_db'))
-
-
-def get_titanic_data(SQL_query, directory, filename="titanic.csv"):
+def check_file_exists(fn, query, url):
     """
-    This function will:
-    - Check local directory for csv file
-        - return if exists
-    - If csv doesn't exists:
-        - create a df of the SQL_query
-        - write df to csv
-    - Output titanic df
-"""
-    if os.path.exists(directory + filename):
-        df = pd.read_csv(filename)
-        return df
-
+    check if file exists in my local directory, if not, pull from sql db
+    return dataframe
+    """
+    if os.path.isfile(fn):
+        print('csv file found and loaded')
+        return pd.read_csv(fn, index_col=0)
     else:
-        df = new_titanic_data(SQL_query)
-
-        # want to save to csv
-        df.to_csv(filename)
+        print('creating df and exporting csv')
+        df = pd.read_sql(query, url)
+        df.to_csv(fn)
         return df
 
-def new_telco_data(sql_query):
-    '''
-        This function reads the titanic data from the Codeup db into a df.
+def get_titanic_data():
+    url = env.get_db_url('titanic_db')
+    filename = 'titanic.csv'
+    query = 'select * from passengers'
+
+    df = check_file_exists(filename, query, url)
+
+    return df
+
+
+def get_iris_data():
+    url = env.get_db_url('iris_db')
+    query = '''
+            select * from measurements
+                join species
+                    using (species_id)
+            '''
+    filename = 'iris.csv'
+    df = check_file_exists(filename, query, url)
+    return df
+
+
+
+def get_telco_churn():
+    url = env.get_db_url('telco_churn')
+    query = ''' select * from customers
+	join contract_types
+		using (contract_type_id)
+	join internet_service_types
+		using (internet_service_type_id)
+	join payment_types
+		using (payment_type_id)
         '''
-    sql_query = 'select c.*, ct.*, ist.*, pt.* from customers c join contract_types ct on c.contract_type_id = ct.contract_type_id join internet_service_types ist on c.internet_service_type_id = ist.internet_service_type_id join payment_types pt on c.payment_type_id = pt.payment_type_id'
-    return pd.read_sql(sql_query, get_db_url('telco_churn'))
+    filename = 'telco.csv'
+    df = check_file_exists(filename, query, url)
 
-def get_telco_data(SQL_query, directory, filename="telco_churn.csv"):
-    """
-    This function will:
-    - Check local directory for csv file
-        - return if exists
-    - If csv doesn't exists:
-        - create a df of the SQL_query
-        - write df to csv
-    - Output titanic df
-"""
-    if os.path.exists(directory + filename):
-        df = pd.read_csv(filename)
-        return df
+    return df
 
-    else:
-        df = new_telco_data(SQL_query)
+def prep_iris(iris):
+    iris = iris.drop(columns=['species_id', 'measurement_id'])
+    iris = iris.rename(columns={'species_name': 'species'})
+    num=iris.select_dtypes(include="number")
+    char=iris.select_dtypes(include="object")
+    char = pd.get_dummies(char)
+    iris_clean = pd.concat([num, char], axis=1)
+    return iris_clean
 
+def prep_telco(telco):
+    telco = telco.drop(columns=['customer_id', 'payment_type_id', 'internet_service_type_id', 'contract_type_id'])
+    num=telco.select_dtypes(include="number")
+    char=telco.select_dtypes(include="object")
+    char = pd.get_dummies(char)
+    telco_clean = pd.concat([num, char], axis=1)
+    return telco_clean
 
-        df.to_csv(filename)
-        return df
+def prep_telco():
+    telco = get_telco_churn()
+    telco = telco.drop(columns=['customer_id', 'payment_type_id', 'internet_service_type_id', 'contract_type_id'])
+    telco['senior_citizen'] = telco['senior_citizen'].replace({0: 'No', 1: 'Yes'})
+    telco['total_charges'] = telco['total_charges'].replace({' ': 0})
+    telco['total_charges'] = telco['total_charges'].astype('float')
+    num=telco.select_dtypes(include="number")
+    char=telco.select_dtypes(include="object")
+    char = pd.get_dummies(char)
+    telco_clean = pd.concat([num, char], axis=1)
+    return telco_clean
 
-def new_iris_data(sql_query):
-    '''
-    This function reads the iris data from the Codeup db into a df.
-    '''
-    sql_query = 'SELECT * FROM measurements JOIN species USING(species_id)'
-    return pd.read_sql(sql_query, get_db_url('iris_db'))
+def test_train(df, target):
 
-def get_iris_data(SQL_query, directory, filename="iris.csv"):
-    if os.path.exists(directory + filename):
-        df = pd.read_csv(filename)
-        return df
-
-    else:
-        df = new_telco_data(SQL_query)
-
-        # want to save to csv
-        df.to_csv(filename)
-        return df
+    temp_train, test = train_test_split(df,  test_size= .2, random_state=123, stratify=df[target])
+    train, validate = train_test_split(temp_train, test_size= .25, random_state=123, stratify=temp_train[target])
+    return train, validate, test
